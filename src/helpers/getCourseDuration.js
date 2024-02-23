29542754a0c2f7c5633690b1711e852bd87a9486
Tamás Petró <petro.tamas@gmail.com>
2024-02-23 14:29:05 +0100

    Duration fix

--- src/helpers/getCourseDuration.js
+++ src/helpers/getCourseDuration.js
@@ -4,5 +4,5 @@ export const getCourseDuration = (duration) => {
   const m = duration % 60;
   const h = (duration - m) / 60;
 
-  return `${padTo2Digits(h)}:${padTo2Digits(m)} hours`;
+  return `${padTo2Digits(h)}:${padTo2Digits(m)} hour${h > 1 ? "s" : ""}`;
 };
